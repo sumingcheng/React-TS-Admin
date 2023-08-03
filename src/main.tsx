0f766ba82a0f828ea5fb125fa0c3b04196986c53
范晓豪 <fanxiaohaohaha@qq.com>
2023-08-03 11:25:00 +0800

    fix: 动态标题

--- src/main.tsx
+++ src/main.tsx
@@ -22,3 +22,5 @@ const Root = () => {
 };
 
 createRoot(document.getElementById('root')).render(<Root/>);
+// 动态设置标题
+document.getElementsByTagName('title')[0].innerHTML = process.env.tabTitle
--- webpack/plugins.js
+++ webpack/plugins.js
@@ -8,7 +8,7 @@ const Plugins = (CONFIG) => {
   return [
     //  浏览器全局变量 使用 process.env.BASE_URL 获取
     new webpack.EnvironmentPlugin({
-      NODE_ENV: CONFIG.mode, // use 'development' unless process.env.NODE_ENV is defined
+      NODE_ENV: CONFIG.mode,
       BASE_URL: CONFIG.BASE_URL, // 接口请求地址
       tabTitle: CONFIG.tabTitle, // 浏览器页签名称
       menuTitle: CONFIG.menuTitle // 菜单名称

7402dbbf64185c4acfc3c5f08dc08f1740327f95
范晓豪 <fanxiaohaohaha@qq.com>
2023-08-03 14:41:58 +0800

    build: 优化打包信息展示

--- webpack/stats.js
+++ webpack/stats.js
@@ -1,12 +1,12 @@
 const Stats = (CONFIG) => {
   return {
-    // all: false, // 默认不显示所有的统计信息
-    // warnings: true, // 显示构建过程中的警告
-    // errors: true, // 显示构建过程中的错误
-    // errorDetails: true, // 显示错误的详细信息
-    // colors: true, // 在控制台中的输出使用颜色区分
-    // performance: true, // 显示性能提示
-    // timings: true // 显示每个模块的构建时间
+    all: false, // 默认不显示所有的统计信息
+    warnings: true, // 显示构建过程中的警告
+    errors: true, // 显示构建过程中的错误
+    errorDetails: true, // 显示错误的详细信息
+    colors: true, // 在控制台中的输出使用颜色区分
+    performance: true, // 显示性能提示
+    timings: true // 显示每个模块的构建时间
   }
 }
 module.exports = Stats

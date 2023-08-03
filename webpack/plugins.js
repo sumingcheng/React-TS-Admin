de7d043f08ee26a27f81c59ef374ab3cf75a91f7
范晓豪 <fanxiaohaohaha@qq.com>
2023-08-03 11:02:56 +0800

    build: 解决生成和开发环境BundleAnalyzerPlugin报错

--- webpack/plugins.js
+++ webpack/plugins.js
@@ -6,32 +6,36 @@ const webpack = require('webpack');
 
 const Plugins = (CONFIG) => {
   return [
-    new BundleAnalyzerPlugin({
-      openAnalyzer: false,
-      analyzerMode: 'static',
-      reportFilename: '../BundleAnalyzer.html'
-    }),
-    new TerserPlugin({
-      extractComments: false
-    }),
-    CONFIG.mode === 'production' &&  new HtmlWebpackPlugin({
-      template: 'public/index.html',  // 模板文件路径
-      filename: 'index.html',  // 输出文件名
-      minify: {
-        collapseWhitespace: true, // 移除空格
-        removeComments: true, // 移除注释
-        removeRedundantAttributes: true // 移除冗余的属性
-      }
-    }),
-    new MiniCssExtractPlugin({
-      filename: 'css/[name].[contenthash].css'
-    }),
     //  浏览器全局变量 使用 process.env.BASE_URL 获取
     new webpack.EnvironmentPlugin({
       NODE_ENV: CONFIG.mode, // use 'development' unless process.env.NODE_ENV is defined
       BASE_URL: CONFIG.BASE_URL, // 接口请求地址
       tabTitle: CONFIG.tabTitle, // 浏览器页签名称
       menuTitle: CONFIG.menuTitle // 菜单名称
+    }),
+    // 在生产打包的时候，启用 BundleAnalyzerPlugin 插件
+    CONFIG.mode === 'production' && new BundleAnalyzerPlugin({
+      openAnalyzer: false,
+      analyzerMode: 'static',
+      reportFilename: '../BundleAnalyzer.html'
+    }),
+    //  压缩js
+    new TerserPlugin({
+      extractComments: false
+    }),
+    // 生成html文件
+    new HtmlWebpackPlugin({
+      template: 'public/index.html',  // 模板文件路径
+      filename: 'index.html',  // 输出文件名
+      minify: { // html 压缩配置
+        collapseWhitespace: true, // 移除空格
+        removeComments: true, // 移除注释
+        removeRedundantAttributes: true // 移除冗余的属性
+      }
+    }),
+    // 提取css
+    new MiniCssExtractPlugin({
+      filename: 'css/[name].[contenthash].css'
     })
   ]
 }

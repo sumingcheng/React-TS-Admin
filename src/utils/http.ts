fef5f38171f86e2e49d490dd04d3707a7d77066e
范晓豪 <fanxiaohaohaha@qq.com>
2023-08-11 08:54:48 +0800

    feat: 优化多次请求控制台报错过多问题

--- src/utils/http.ts
+++ src/utils/http.ts
@@ -4,6 +4,7 @@ import {message} from 'antd'
 // 定义请求信息的接口
 interface RequestInfo {
   timestamp: number; // 请求的时间戳
+  count: number;     // 请求的次数
 }
 
 // 创建axios实例并设置默认配置
@@ -27,32 +28,38 @@ const generateRequestKey = (config: AxiosRequestConfig) => {
 // 请求拦截器
 AxiosInstances.interceptors.request.use(
     config => {
-      const key = generateRequestKey(config) // 获取请求的键值
-      const now = Date.now()                 // 获取当前时间戳
+      const key = generateRequestKey(config)
+      const now = Date.now()
 
-      const info = requestMap.get(key)       // 查找该请求是否在映射中
+      const info = requestMap.get(key)
 
       // 如果请求存在，并且在1秒内重复发起
       if (info && (now - info.timestamp < 1000)) {
-        message.warning('请不要重复请求')
-        return Promise.reject(new Error('重复请求'))
+        info.count++  // 增加请求次数
+        requestMap.set(key, info) // 更新请求信息
+
+        if (info.count > 2) {  // 如果是第二次或更多次重复请求
+          message.warning('请不要重复请求')
+          return Promise.reject('请不要重复请求')
+        } else {
+          return config
+        }
+
       }
 
       // 如果请求不存在于映射中
       if (!info) {
-        requestMap.set(key, {timestamp: now})      // 将请求添加到映射中
+        requestMap.set(key, {timestamp: now, count: 1})  // 初始请求次数设置为1
 
-        // 1秒后从映射中移除该请求
         setTimeout(() => {
           requestMap.delete(key)
         }, 1000)
       }
 
-      return config  // 返回请求配置
+      return config
     },
     error => Promise.reject(error),
 )
-
 // 响应拦截器
 AxiosInstances.interceptors.response.use(
     res => {
@@ -67,9 +74,6 @@ AxiosInstances.interceptors.response.use(
       // 如果请求超时
       if (error.code === 'ECONNABORTED') {
         message.warning('请求超时，请稍后重试')
-      } else {
-        // 其他请求错误
-        message.error('请求失败，请稍后重试')
       }
 
       return Promise.reject(error)

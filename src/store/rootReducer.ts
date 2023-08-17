74c9486875003e6316dd0043f484a484380bb824
范晓豪 <fanxiaohaohaha@qq.com>
2023-08-17 11:09:08 +0800

    build: 规范指令

--- src/store/rootReducer.ts
+++ src/store/rootReducer.ts
@@ -1,7 +1,6 @@
 // combineReducers 可以将多个 reducer 函数合并成一个 rootReducer
-import { combineReducers } from "@reduxjs/toolkit"
-
-import themeReducer from "./themeSlice"
+import {combineReducers} from '@reduxjs/toolkit'
+import themeReducer from './themeSlice'
 
 const rootReducer = combineReducers({
   // 这意味着你可以使用 state.theme 来访问 themeReducer 管理的状态
